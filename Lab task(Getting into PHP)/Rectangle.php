d31f0807f1d52083406c1b16bcf01cfe452b58bf
Ridwan Rafin <159000194+RidwanRafin@users.noreply.github.com>
2024-02-15 13:54:07 +0600

    Add files via upload

---
+++ Lab task(Getting into PHP)/Rectangle.php
@@ -0,0 +1,11 @@
+<?php
+
+$length = 10;
+$width = 5;
+$area = $length * $width;
+$perimeter = 2 * ($length + $width);
+
+echo "Rectangle Area: " . $area . "square units ";
+echo "Rectangle Perimeter: " . $perimeter . " units ";
+
+?>
